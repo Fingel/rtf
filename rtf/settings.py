8b2bf7f0760a38f149d84306e3b264a0cc6dab87
100010001 <100010001@anonymousspeech.com>
2013-06-11 13:43:08 -0500

    removed old templates. added rtf_theme

--- rtf/settings.py
+++ rtf/settings.py
@@ -212,6 +212,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "rtf_theme",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
