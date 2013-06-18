cb5e2a50fe2d726122e3243347a285e6e2218b69
Austin Riba <austin.riba@gmail.com>
2013-06-17 22:49:37 -0700

    disable debug

--- rtf/settings.py
+++ rtf/settings.py
@@ -113,7 +113,7 @@ LANGUAGE_CODE = "en"
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
-DEBUG = os.environ.get('DEBUG', "")
+DEBUG = False
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
