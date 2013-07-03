60aeddf487931f7006b1466b6ab9dffc5f6951f1
Austin Riba <austin.riba@gmail.com>
2013-07-03 08:30:19 -0700

    merge

--- rtf/views.py
+++ rtf/views.py
@@ -1,27 +1,41 @@
 from django.http import HttpResponse
 from django.template import Context, loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from rtf.models import Protest
 from geopy import geocoders
 from django.contrib.admin.views.decorators import staff_member_required
-from time import sleep
-from django.utils import simplejson
 from django.core import serializers
+import json
+
+import logging
+log = logging.getLogger(__name__)
+
+def deslugify(text):
+	return text.title().replace('-', ' ')
 
 def protests(request):
 	protest_list = Protest.objects.all().order_by('city')
 	context = {'protest_list' : protest_list}
 	return render(request, 'protests/protests.html', context)
 
-def protest(request, protest_id):
-	protest = get_object_or_404(Protest, pk=protest_id)
-	context = {'protest' : protest }
-	return render(request, 'protests/protest.html', context)
+def protests_by_state(request, state=None):
+	protests = get_list_or_404(Protest, state=deslugify(state))
+	return render(request, 'protests/protest.html', {'protests': protests})
 
-def protestsjson(request):
-	protest_list = Protest.objects.all()
-	data = serializers.serialize('json', protest_list)
-	return HttpResponse(data, mimetype="application/json")
+def protest_by_city(request, state=None, city=None, protest=None):
+	if protest is None:
+		log.debug("NOOOOOOOOOOOOO!!!")
+		protest = get_object_or_404(Protest, state=deslugify(state), city=deslugify(city))
+	return render(request, 'protests/protest.html', {'protest': protest})
+
+def protest_by_id(request, protest_id=None):
+	protest = get_object_or_404(Protest, pk=protest_id)
+	return redirect(protest.get_absolute_url(), protest=protest)
+
+def protests_json(request):
+	protest_list = Protest.objects.all().exclude(state=None).order_by('-state')
+	data = [protest.to_json() for protest in protest_list]
+	return HttpResponse(json.dumps(data), mimetype="application/json")
 
 @staff_member_required
 def recalclatlong(request):
@@ -31,6 +45,6 @@ def recalclatlong(request):
 		protest.save()
 	context = {'protest_list' : protests}
 	return render(request, 'protests/protests.html', context)
-	
+
 def blitzio(request):
 	return HttpResponse("42")
